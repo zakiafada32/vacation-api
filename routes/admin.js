880eae2818fb73203eebecedd7493edf0a02fbb3
zakiafada32 <afadazaki32@gmail.com>
2020-10-13 13:54:41 +0700

    feature crud

--- routes/admin.js
+++ routes/admin.js
@@ -22,6 +22,15 @@ router.get('/item/show-image/:id', adminController.showImageItem);
 router.get('/item/:id', adminController.showEditItem);
 router.put('/item/:id', uploadMultiple, adminController.editItem);
 router.delete('/item/:id/delete', adminController.deleteItem);
+// endpoint detail item
+router.get('/item/show-detail-item/:itemId', adminController.viewDetailItem);
+router.post('/item/add/feature', uploadSingle, adminController.addFeature);
+router.put('/item/update/feature', uploadSingle, adminController.editFeature);
+router.delete(
+  '/item/:itemId/feature/:id',
+  uploadSingle,
+  adminController.deleteFeature
+);
 // endpoint booking
 router.get('/booking', adminController.viewBooking);
 
